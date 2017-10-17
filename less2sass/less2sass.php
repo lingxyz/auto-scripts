1241f699a03e4853a165e19992df12cb184a090e
Ling Zhang <zhangling.rd@gmail.com>
2017-10-17 20:52:31 +0800

    .editorconfig统一Git代码格式

--- less2sass/less2sass.php
+++ less2sass/less2sass.php
@@ -1,17 +1,17 @@
 <?php
 // 将 less 转化为 sass， 并在 bash 输出
-    $less = './test.less';
-    $sass = './test.sass';
-    if (file_exists($sass)) {
-        unlink ('./test.sass');
-    }
-    $lessFile = fopen("test.less", "r+") or exit("无法打开文件!");
-    $sassFile = fopen("test.sass", "x+") or exit("无法创建文件!");
-    while(!feof($lessFile))
-    {
-        $replaceString = preg_replace('/\{|\}|\;/', '', fgets($lessFile));
-        echo $replaceString;
-        fwrite($sassFile, $replaceString);
-    }
-    fclose($lessFile);
-    fclose($sassFile);
+	$less = './test.less';
+	$sass = './test.sass';
+	if (file_exists($sass)) {
+		unlink ('./test.sass');
+	}
+	$lessFile = fopen("test.less", "r+") or exit("无法打开文件!");
+	$sassFile = fopen("test.sass", "x+") or exit("无法创建文件!");
+	while(!feof($lessFile))
+	{
+		$replaceString = preg_replace('/\{|\}|\;/', '', fgets($lessFile));
+		echo $replaceString;
+		fwrite($sassFile, $replaceString);
+	}
+	fclose($lessFile);
+	fclose($sassFile);
